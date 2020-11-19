aa201a2fcd3f81a8b6dda306e4f22e40fc3a610f
kotomasakashiwagi <73920282+kotomasakashiwagi@users.noreply.github.com>
2020-11-19 18:35:15 +0900

    Initial commit

---
+++ src/main/java/QuestionSearcher.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class QuestionSearcher {
+    private static final Map<String, Question> questionMap = new HashMap<String, Question>() {
+        {
+           // put("5-1", new Question1());
+            //put("5-2", new Question2());
+            //put("5-3", new Question3());
+            //put("5-4", new Question4());
+            //put("5-5", new Question5());
+            //put("5-6", new Question6());
+            //put("5-7", new Question7());
+            //put("5-8", new Question8());
+            //put("err", new OutOfQuestion());
+        }
+    };
+    public void selectQuestion(String QuestionNumber){
+
+    }
+
+}
